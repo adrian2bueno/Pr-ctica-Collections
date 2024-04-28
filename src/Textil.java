136b95d0a61899d6d73e29a8511a7266eb5db3b1
adrianbueno <a.bueno2@sapalomera.cat>
2024-04-28 22:07:46 +0200

    Comparable

--- src/Textil.java
+++ src/Textil.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Textil extends Producte implements Comparable<Textil>{
     private String composicio;
 
-    //
     public Textil(String nom, float preu, String codibarres, String composicio) {
         super(nom, preu, codibarres);
         this.composicio = composicio;
@@ -16,15 +17,17 @@ public class Textil extends Producte implements Comparable<Textil>{
     public void setComposicio(String composicio) {
         this.composicio = composicio;
     }
+
     public int compareTo(Textil t){
-        //TODO
+
         return this.getComposicio().compareTo(t.getComposicio());
     }
+    //TODO
     public final boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Textil textil)) return false;
         if (!super.equals(o)) return false;
 
-        return Objects.equals(getCodibarres(), textil.getCodibarres());
+        return Objects.equals(getCodiBarres(), textil.getCodiBarres());
     }
 }
